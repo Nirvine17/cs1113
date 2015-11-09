d34a0fb6863fcf907debab14b004da3a29d26782
Nick Irvine <nji8fd@virginia.edu>
2015-11-08 19:33:20 -0500

    added examples.py

---
+++ examples.py
@@ -0,0 +1,35 @@
+# assignment commands
+
+y = 5
+x = 6
+
+# conditions
+
+if (True):
+    x = 0
+    z = 10
+else:
+    x = 1
+    x = 11
+
+
+if (x > 0 or y  == x):
+    x = 0
+    z = 10
+else:
+    x = 1
+    x = 11
+
+# iteration
+
+times = 10
+while (times > 0):
+    print("Hello")
+    times = (times - 1)
+
+
+#procedures and procedure calls
+
+def fun(a, b, c, x):
+    result = a * (x * x) + b * x + c
+    return result
